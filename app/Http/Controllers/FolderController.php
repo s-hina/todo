8769f181c087270c3fc46e672c10d324b2fae812
h-saitocm <1770hina@gmail.com>
2020-09-07 18:13:32 +0900

    フォルダ作成

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Folder;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateFolder;
 
 class FolderController extends Controller
 {
     public function showCreateForm()
     {
         return view('folders/create');
+    }
+
+    public function create(CreateFolder $request)
+    {
+        //フォルダモデルのインスタンスを作成
+        $folder = new Folder();
+        //タイトルに入力値を代入
+        $folder->title = $request->title;
+        //インスタンスの状態をデータベースに書き込む
+        $folder->save();
+
+        return redirect()->route('tasks.index',[
+            'id' => $folder->id,
+        ]);
     }
 }
